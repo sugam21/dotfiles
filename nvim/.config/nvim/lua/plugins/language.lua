abf8dd76df7e59194ba06924478ca0a3192acbb7
Sugam <sugams342@gmail.com>
2023-11-22 13:25:48 +0530

    added language support
    - python
    - java
    - typescript
    - cpp
    - go
    - tailwind
    - docker

--- nvim/.config/nvim/lua/plugins/language.lua
+++ nvim/.config/nvim/lua/plugins/language.lua
@@ -1,85 +1,400 @@
--- TREESITTER
-return {
-  {
-    "nvim-treesitter/nvim-treesitter",
-    opts = function(_, opts)
-      if type(opts.ensure_installed) == "table" then
-        vim.list_extend(opts.ensure_installed, { "ninja", "python", "rst", "toml" })
-      end
-    end,
-  },
-},
--- LSP
-{
-  "neovim/nvim-lspconfig",
-  opts = {
-    servers = {
-      pyright = {},
-      ruff_lsp = {
-        keys = {
-          {
-            "<leader>co",
-            function()
-              vim.lsp.buf.code_action({
-                apply = true,
-                context = {
-                  only = { "source.organizeImports" },
-                  diagnostics = {},
-                },
-              })
-            end,
-            desc = "Organize Imports",
-          },
-        },
-      },
-    },
-    setup = {
-      ruff_lsp = function()
-        require("lazyvim.util").lsp.on_attach(function(client, _)
-          if client.name == "ruff_lsp" then
-            -- Disable hover in favor of Pyright
-            client.server_capabilities.hoverProvider = false
-          end
-        end)
-      end,
-    },
-  },
-  -- NEOTEST
-  {
-    "nvim-neotest/neotest-python",
-  },
-
-  -- NVIM DAP
-  {
-    "mfussenegger/nvim-dap-python",
-  -- stylua: ignore
-  keys = {
-    { "<leader>dPt", function() require('dap-python').test_method() end, desc = "Debug Method", ft = "python" },
-    { "<leader>dPc", function() require('dap-python').test_class() end, desc = "Debug Class", ft = "python" },
-  },
-    config = function()
-      local path = require("mason-registry").get_package("debugpy"):get_install_path()
-      require("dap-python").setup(path .. "/venv/bin/python")
-    end,
-  },
-
-  -- VENV SELECTOR
-  {
-    "linux-cultist/venv-selector.nvim",
-    cmd = "VenvSelect",
-    opts = function(_, opts)
-      if require("lazyvim.util").has("nvim-dap-python") then
-        opts.dap_enabled = true
-      end
-      return vim.tbl_deep_extend("force", opts, {
-        name = {
-          "venv",
-          ".venv",
-          "env",
-          ".env",
-        },
-      })
-    end,
-    keys = { { "<leader>cv", "<cmd>:VenvSelect<cr>", desc = "Select VirtualEnv" } },
-  },
-}
+return {}
+-- -- TREESITTER
+--
+-- return {
+--   {
+--     "nvim-treesitter/nvim-treesitter",
+--     opts = function(_, opts)
+--       if type(opts.ensure_installed) == "table" then
+--         vim.list_extend(opts.ensure_installed, { "ninja", "java", "c", "cpp", "python", "rst", "toml" })
+--       end
+--     end,
+--   },
+-- },
+-- -- CLANG-D
+-- {
+--   "p00f/clangd_extensions.nvim",
+--   lazy = true,
+--   config = function() end,
+--   opts = {
+--     inlay_hints = {
+--       inline = false,
+--     },
+--     ast = {
+--       --These require codicons (https://github.com/microsoft/vscode-codicons)
+--       role_icons = {
+--         type = "",
+--         declaration = "",
+--         expression = "",
+--         specifier = "",
+--         statement = "",
+--         ["template argument"] = "",
+--       },
+--       kind_icons = {
+--         Compound = "",
+--         Recovery = "",
+--         TranslationUnit = "",
+--         PackExpansion = "",
+--         TemplateTypeParm = "",
+--         TemplateTemplateParm = "",
+--         TemplateParamObject = "",
+--       },
+--     },
+--   },
+-- },
+-- -- LSP
+-- {
+--   "neovim/nvim-lspconfig",
+--   opts = {
+--     servers = {
+--       -- JAVA
+--       jdtls = {},
+--       -- PYRIGHT
+--       pyright = {},
+--       -- CLANG-D
+--       clangd = {
+--         keys = {
+--           { "<leader>cR", "<cmd>ClangdSwitchSourceHeader<cr>", desc = "Switch Source/Header (C/C++)" },
+--         },
+--         root_dir = function(fname)
+--           return require("lspconfig.util").root_pattern(
+--             "Makefile",
+--             "configure.ac",
+--             "configure.in",
+--             "config.h.in",
+--             "meson.build",
+--             "meson_options.txt",
+--             "build.ninja"
+--           )(fname) or require("lspconfig.util").root_pattern("compile_commands.json", "compile_flags.txt")(
+--             fname
+--           ) or require("lspconfig.util").find_git_ancestor(fname)
+--         end,
+--         capabilities = {
+--           offsetEncoding = { "utf-16" },
+--         },
+--         cmd = {
+--           "clangd",
+--           "--background-index",
+--           "--clang-tidy",
+--           "--header-insertion=iwyu",
+--           "--completion-style=detailed",
+--           "--function-arg-placeholders",
+--           "--fallback-style=llvm",
+--         },
+--         init_options = {
+--           usePlaceholders = true,
+--           completeUnimported = true,
+--           clangdFileStatus = true,
+--         },
+--       },
+--       ruff_lsp = {
+--         keys = {
+--           {
+--             "<leader>co",
+--             function()
+--               vim.lsp.buf.code_action({
+--                 apply = true,
+--                 context = {
+--                   only = { "source.organizeImports" },
+--                   diagnostics = {},
+--                 },
+--               })
+--             end,
+--             desc = "Organize Imports",
+--           },
+--         },
+--       },
+--     },
+--     setup = {
+--       ruff_lsp = function()
+--         require("lazyvim.util").lsp.on_attach(function(client, _)
+--           if client.name == "ruff_lsp" then
+--             -- Disable hover in favor of Pyright
+--             client.server_capabilities.hoverProvider = false
+--           end
+--         end)
+--       end,
+--       clangd = function(_, opts)
+--         local clangd_ext_opts = require("lazyvim.util").opts("clangd_extensions.nvim")
+--         require("clangd_extensions").setup(vim.tbl_deep_extend("force", clangd_ext_opts or {}, { server = opts }))
+--         return false
+--       end,
+--       jdtls = function()
+--         return true -- avoid duplicate servers
+--       end,
+--     },
+--   },
+--   -- NVIM-JTLD
+--   {
+--     "mfussenegger/nvim-jdtls",
+--     dependencies = { "folke/which-key.nvim" },
+--     ft = java_filetypes,
+--     opts = function()
+--       return {
+--         -- How to find the root dir for a given filename. The default comes from
+--         -- lspconfig which provides a function specifically for java projects.
+--         root_dir = require("lspconfig.server_configurations.jdtls").default_config.root_dir,
+--
+--         -- How to find the project name for a given root dir.
+--         project_name = function(root_dir)
+--           return root_dir and vim.fs.basename(root_dir)
+--         end,
+--
+--         -- Where are the config and workspace dirs for a project?
+--         jdtls_config_dir = function(project_name)
+--           return vim.fn.stdpath("cache") .. "/jdtls/" .. project_name .. "/config"
+--         end,
+--         jdtls_workspace_dir = function(project_name)
+--           return vim.fn.stdpath("cache") .. "/jdtls/" .. project_name .. "/workspace"
+--         end,
+--
+--         -- How to run jdtls. This can be overridden to a full java command-line
+--         -- if the Python wrapper script doesn't suffice.
+--         cmd = { vim.fn.exepath("jdtls") },
+--         full_cmd = function(opts)
+--           local fname = vim.api.nvim_buf_get_name(0)
+--           local root_dir = opts.root_dir(fname)
+--           local project_name = opts.project_name(root_dir)
+--           local cmd = vim.deepcopy(opts.cmd)
+--           if project_name then
+--             vim.list_extend(cmd, {
+--               "-configuration",
+--               opts.jdtls_config_dir(project_name),
+--               "-data",
+--               opts.jdtls_workspace_dir(project_name),
+--             })
+--           end
+--           return cmd
+--         end,
+--
+--         -- These depend on nvim-dap, but can additionally be disabled by setting false here.
+--         dap = { hotcodereplace = "auto", config_overrides = {} },
+--         test = true,
+--       }
+--     end,
+--     config = function()
+--       local opts = Util.opts("nvim-jdtls") or {}
+--
+--       -- Find the extra bundles that should be passed on the jdtls command-line
+--       -- if nvim-dap is enabled with java debug/test.
+--       local mason_registry = require("mason-registry")
+--       local bundles = {} ---@type string[]
+--       if opts.dap and Util.has("nvim-dap") and mason_registry.is_installed("java-debug-adapter") then
+--         local java_dbg_pkg = mason_registry.get_package("java-debug-adapter")
+--         local java_dbg_path = java_dbg_pkg:get_install_path()
+--         local jar_patterns = {
+--           java_dbg_path .. "/extension/server/com.microsoft.java.debug.plugin-*.jar",
+--         }
+--         -- java-test also depends on java-debug-adapter.
+--         if opts.test and mason_registry.is_installed("java-test") then
+--           local java_test_pkg = mason_registry.get_package("java-test")
+--           local java_test_path = java_test_pkg:get_install_path()
+--           vim.list_extend(jar_patterns, {
+--             java_test_path .. "/extension/server/*.jar",
+--           })
+--         end
+--         for _, jar_pattern in ipairs(jar_patterns) do
+--           for _, bundle in ipairs(vim.split(vim.fn.glob(jar_pattern), "\n")) do
+--             table.insert(bundles, bundle)
+--           end
+--         end
+--       end
+--
+--       local function attach_jdtls()
+--         local fname = vim.api.nvim_buf_get_name(0)
+--
+--         -- Configuration can be augmented and overridden by opts.jdtls
+--         local config = extend_or_override({
+--           cmd = opts.full_cmd(opts),
+--           root_dir = opts.root_dir(fname),
+--           init_options = {
+--             bundles = bundles,
+--           },
+--           -- enable CMP capabilities
+--           capabilities = require("cmp_nvim_lsp").default_capabilities(),
+--         }, opts.jdtls)
+--
+--         -- Existing server will be reused if the root_dir matches.
+--         require("jdtls").start_or_attach(config)
+--         -- not need to require("jdtls.setup").add_commands(), start automatically adds commands
+--       end
+--
+--       -- Attach the jdtls for each java buffer. HOWEVER, this plugin loads
+--       -- depending on filetype, so this autocmd doesn't run for the first file.
+--       -- For that, we call directly below.
+--       vim.api.nvim_create_autocmd("FileType", {
+--         pattern = java_filetypes,
+--         callback = attach_jdtls,
+--       })
+--
+--       -- Setup keymap and dap after the lsp is fully attached.
+--       -- https://github.com/mfussenegger/nvim-jdtls#nvim-dap-configuration
+--       -- https://neovim.io/doc/user/lsp.html#LspAttach
+--       vim.api.nvim_create_autocmd("LspAttach", {
+--         callback = function(args)
+--           local client = vim.lsp.get_client_by_id(args.data.client_id)
+--           if client and client.name == "jdtls" then
+--             local wk = require("which-key")
+--             wk.register({
+--               ["<leader>cx"] = { name = "+extract" },
+--               ["<leader>cxv"] = { require("jdtls").extract_variable_all, "Extract Variable" },
+--               ["<leader>cxc"] = { require("jdtls").extract_constant, "Extract Constant" },
+--               ["gs"] = { require("jdtls").super_implementation, "Goto Super" },
+--               ["gS"] = { require("jdtls.tests").goto_subjects, "Goto Subjects" },
+--               ["<leader>co"] = { require("jdtls").organize_imports, "Organize Imports" },
+--             }, { mode = "n", buffer = args.buf })
+--             wk.register({
+--               ["<leader>c"] = { name = "+code" },
+--               ["<leader>cx"] = { name = "+extract" },
+--               ["<leader>cxm"] = {
+--                 [[<ESC><CMD>lua require('jdtls').extract_method(true)<CR>]],
+--                 "Extract Method",
+--               },
+--               ["<leader>cxv"] = {
+--                 [[<ESC><CMD>lua require('jdtls').extract_variable_all(true)<CR>]],
+--                 "Extract Variable",
+--               },
+--               ["<leader>cxc"] = {
+--                 [[<ESC><CMD>lua require('jdtls').extract_constant(true)<CR>]],
+--                 "Extract Constant",
+--               },
+--             }, { mode = "v", buffer = args.buf })
+--
+--             if opts.dap and Util.has("nvim-dap") and mason_registry.is_installed("java-debug-adapter") then
+--               -- custom init for Java debugger
+--               require("jdtls").setup_dap(opts.dap)
+--               require("jdtls.dap").setup_dap_main_class_configs()
+--
+--               -- Java Test require Java debugger to work
+--               if opts.test and mason_registry.is_installed("java-test") then
+--                 -- custom keymaps for Java test runner (not yet compatible with neotest)
+--                 wk.register({
+--                   ["<leader>t"] = { name = "+test" },
+--                   ["<leader>tt"] = { require("jdtls.dap").test_class, "Run All Test" },
+--                   ["<leader>tr"] = { require("jdtls.dap").test_nearest_method, "Run Nearest Test" },
+--                   ["<leader>tT"] = { require("jdtls.dap").pick_test, "Run Test" },
+--                 }, { mode = "n", buffer = args.buf })
+--               end
+--             end
+--
+--             -- User can set additional keymaps in opts.on_attach
+--             if opts.on_attach then
+--               opts.on_attach(args)
+--             end
+--           end
+--         end,
+--       })
+--
+--       -- Avoid race condition by calling attach the first time, since the autocmd won't fire.
+--       attach_jdtls()
+--     end,
+--   },
+--   -- NEOTEST
+--   {
+--     "nvim-neotest/neotest-python",
+--   },
+--
+--   -- NVIM DAP PYTHON
+--   {
+--     "mfussenegger/nvim-dap-python",
+--   -- stylua: ignore
+--   keys = {
+--     { "<leader>dPt", function() require('dap-python').test_method() end, desc = "Debug Method", ft = "python" },
+--     { "<leader>dPc", function() require('dap-python').test_class() end, desc = "Debug Class", ft = "python" },
+--   },
+--     config = function()
+--       local path = require("mason-registry").get_package("debugpy"):get_install_path()
+--       require("dap-python").setup(path .. "/venv/bin/python")
+--     end,
+--   },
+--   -- MASON
+--   {
+--     -- Ensure C/C++ debugger is installed
+--     "williamboman/mason.nvim",
+--     optional = true,
+--     opts = function(_, opts)
+--       if type(opts.ensure_installed) == "table" then
+--         vim.list_extend(opts.ensure_installed, { "codelldb", "java-test", "java-debug-adapter" })
+--       end
+--     end,
+--   },
+--   -- NVIM DAP
+--   {
+--     "mfussenegger/nvim-dap",
+--     optional = true,
+--     dependencies = {
+--       -- Ensure C/C++ debugger is installed
+--       "williamboman/mason.nvim",
+--       optional = true,
+--       opts = function(_, opts)
+--         if type(opts.ensure_installed) == "table" then
+--           vim.list_extend(opts.ensure_installed, { "codelldb", "java-test", "java-debug-adapter" })
+--         end
+--       end,
+--     },
+--     opts = function()
+--       local dap = require("dap")
+--       if not dap.adapters["codelldb"] then
+--         require("dap").adapters["codelldb"] = {
+--           type = "server",
+--           host = "localhost",
+--           port = "${port}",
+--           executable = {
+--             command = "codelldb",
+--             args = {
+--               "--port",
+--               "${port}",
+--             },
+--           },
+--         }
+--       end
+--       for _, lang in ipairs({ "c", "cpp" }) do
+--         dap.configurations[lang] = {
+--           {
+--             type = "codelldb",
+--             request = "launch",
+--             name = "Launch file",
+--             program = function()
+--               return vim.fn.input("Path to executable: ", vim.fn.getcwd() .. "/", "file")
+--             end,
+--             cwd = "${workspaceFolder}",
+--           },
+--           {
+--             type = "codelldb",
+--             request = "attach",
+--             name = "Attach to process",
+--             processId = require("dap.utils").pick_process,
+--             cwd = "${workspaceFolder}",
+--           },
+--         }
+--       end
+--     end,
+--   },
+--   -- VENV SELECTOR
+--   {
+--     "linux-cultist/venv-selector.nvim",
+--     cmd = "VenvSelect",
+--     opts = function(_, opts)
+--       if require("lazyvim.util").has("nvim-dap-python") then
+--         opts.dap_enabled = true
+--       end
+--       return vim.tbl_deep_extend("force", opts, {
+--         name = {
+--           "venv",
+--           ".venv",
+--           "env",
+--           ".env",
+--         },
+--       })
+--     end,
+--     keys = { { "<leader>cv", "<cmd>:VenvSelect<cr>", desc = "Select VirtualEnv" } },
+--   },
+--
+--   --NVIM CMP
+--   {
+--     "nvim-cmp",
+--     opts = function(_, opts)
+--       table.insert(opts.sorting.comparators, 1, require("clangd_extensions.cmp_scores"))
+--     end,
+--   },
+-- }
