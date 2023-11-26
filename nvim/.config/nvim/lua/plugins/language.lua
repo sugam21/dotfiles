f3e41730d87ba0866e132e78fe53c44612bd96eb
Sugam <sugams342@gmail.com>
2023-11-27 00:04:59 +0530

    added molten.nvim

--- nvim/.config/nvim/lua/plugins/language.lua
+++ nvim/.config/nvim/lua/plugins/language.lua
@@ -1,4 +1,15 @@
-return {}
+return {
+  {
+    "benlubas/molten-nvim",
+    version = "^1.0.0", -- use version <2.0.0 to avoid breaking changes
+    build = ":UpdateRemotePlugins",
+    init = function()
+      -- these are examples, not defaults. Please see the readme
+      vim.g.molten_output_win_max_height = 20
+      vim.g.molten_auto_open_output = false
+    end,
+  },
+}
 -- -- TREESITTER
 --
 -- return {
