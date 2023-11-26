4ab2aa5bceae6b8f329e4a2b2389bc000b837efc
Sugam <sugams342@gmail.com>
2023-11-26 23:28:48 +0530

    shrunk the font and added background image

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -14,21 +14,21 @@ return {
 	},
 	color_scheme = "customcatppuccin",
 	enable_tab_bar = false,
-	font_size = 15.0,
+	font_size = 14.6,
 	font = wezterm.font("JetbrainsMonoNL Nerd Font"),
 
-	window_decorations = "RESIZE",
-	-- macos_window_background_blur = 40,
+	--	window_decorations = "RESIZE",
+	macos_window_background_blur = 40,
 	-- macos_window_background_blur = 30,
 
-	-- window_background_image = '/Users/omerhamerman/Downloads/3840x1080-Wallpaper-041.jpg',
-	-- window_background_image_hsb = {
-	-- 	brightness = 0.01,
-	-- 	hue = 1.0,
-	-- 	saturation = 0.5,
-	-- },
-	-- window_background_opacity = 0.92,
-	window_background_opacity = 1,
+	window_background_image = "/home/sugam/Pictures/Wallpapers/terminal_background.jpg",
+	window_background_image_hsb = {
+		brightness = 0.5,
+		hue = 1.0,
+		saturation = 1,
+	},
+	window_background_opacity = 0.92,
+	--window_background_opacity = 1,
 	-- window_background_opacity = 0.78,
 	-- window_background_opacity = 0.20,
 	keys = {
