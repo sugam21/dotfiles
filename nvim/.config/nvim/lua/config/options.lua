9f8714bb372044b27dc703e00946acdb805f4d52
Sugam <sugams342@gmail.com>
2023-12-02 23:16:35 +0530

    added cursor line

--- nvim/.config/nvim/lua/config/options.lua
+++ nvim/.config/nvim/lua/config/options.lua
@@ -7,9 +7,9 @@ vim.g.mapleader = " "
 vim.scriptencoding = "utf-8"
 vim.opt.encoding = "utf-8"
 vim.opt.fileencoding = "utf-8"
-
+-- Disable the line behind cursor
+vim.opt.cursorline = false
 vim.opt.number = true
-
 vim.opt.title = true
 vim.opt.autoindent = true
 vim.opt.smartindent = true
