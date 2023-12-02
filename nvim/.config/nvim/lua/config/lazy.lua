87a1f46a267c5a3039c77a85276fd1ff0bf68d07
Sugam <sugams342@gmail.com>
2023-12-02 22:38:40 +0530

    --

--- nvim/.config/nvim/lua/config/lazy.lua
+++ nvim/.config/nvim/lua/config/lazy.lua
@@ -15,7 +15,7 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.lang.json" },
     { import = "lazyvim.plugins.extras.lang.clangd" },
     -- { import = "lazyvim.plugins.extras.lang.java" },
-    { import = "lazyvim.plugins.extras.formatting.black" },
+    --{ import = "lazyvim.plugins.extras.formatting.black" },
     { import = "lazyvim.plugins.extras.formatting.prettier" },
     { import = "lazyvim.plugins.extras.lang.json" },
     { import = "lazyvim.plugins.extras.lang.markdown" },
--- nvim/.config/nvim/lua/plugins/language.lua
+++ nvim/.config/nvim/lua/plugins/language.lua
@@ -1,31 +1,10 @@
 return {
 
-  -- this taps into vim.ui.select and vim.ui.input
-  -- and in doing so currently breaks renaming in otter.nvim
   { "stevearc/dressing.nvim", enabled = false },
 
   {
     "quarto-dev/quarto-nvim",
     opts = {
-      --      lspFeatures = {
-      --        enabled = true,
-      --        languages = { "r", "python", "julia", "bash", "html", "lua" },
-      --        chunks = "curly", -- 'curly' or 'all'
-      --        diagnostics = {
-      --          enabled = true,
-      --          triggers = { "BufWritePost" },
-      --        },
-      -- codeRunner = {
-      --      enabled = false,
-      --      default_method = nil, -- 'molten' or 'slime'
-      --      ft_runners = {}, -- filetype to runner, ie. `{ python = "molten" }`.
-      --                       -- Takes precedence over `default_method`
-      --      never_run = { "yaml" }, -- filetypes which are never sent to a code runner
-      --    },
-      --    completion = {
-      --      enabled = true
-      --      },
-      --
       lspFeatures = {
         enabled = true,
         languages = { "r", "python", "julia", "bash", "html", "lua" },
@@ -77,22 +56,22 @@ return {
     },
   },
 
-  {
-    "hrsh7th/nvim-cmp",
-    dependencies = {
-      { "jmbuhr/otter.nvim" },
-      { "jc-doyle/cmp-pandoc-references" },
-      { "kdheepak/cmp-latex-symbols" },
-      { "ray-x/cmp-treesitter" },
-      { "f3fora/cmp-spell" },
-    },
-    opts = function(_, opts)
-      ---@param opts cmp.ConfigSchema
-      local cmp = require("cmp")
-      opts.sources = cmp.config.sources(vim.list_extend(opts.sources, { { name = "otter" } }))
-    end,
-  },
-
+  -- {
+  --   "hrsh7th/nvim-cmp",
+  --   dependencies = {
+  --     { "jmbuhr/otter.nvim" },
+  --     { "jc-doyle/cmp-pandoc-references" },
+  --     { "kdheepak/cmp-latex-symbols" },
+  --     { "ray-x/cmp-treesitter" },
+  --     { "f3fora/cmp-spell" },
+  --   },
+  --   opts = function(_, opts)
+  --     ---@param opts cmp.ConfigSchema
+  --     local cmp = require("cmp")
+  --     opts.sources = cmp.config.sources(vim.list_extend(opts.sources, { { name = "otter" } }))
+  --   end,
+  -- },
+  --
   -- send code from python/r/qmd documets to a terminal or REPL
   -- like ipython, R, bash
   {
@@ -135,28 +114,61 @@ return {
     end,
   },
 
-  {
-    "neovim/nvim-lspconfig",
-    ---@class PluginLspOpts
-    opts = {
-      ---@type lspconfig.options
-      servers = {
-        clangd = {},
-        -- pyright = {},
-        -- r_language_server = {},
-        --julials = {},
-        marksman = {
-          -- also needs:
-          -- $home/.config/marksman/config.toml :
-          -- [core]
-          -- markdown.file_extensions = ["md", "markdown", "qmd"]
-          filetypes = { "markdown", "quarto" },
-          root_dir = require("lspconfig.util").root_pattern(".git", ".marksman.toml", "_quarto.yml"),
-        },
-      },
-    },
-  },
+  -- {
+  --   "neovim/nvim-lspconfig",
+  --   ---@class PluginLspOpts
+  --   opts = {
+  --     ---@type lspconfig.options
+  --     servers = {
+  --       -- clangd = {},
+  --       -- pyright = {},
+  --       -- r_language_server = {},
+  --       -- julials = {},
+  --       marksman = {
+  --         -- also needs:
+  --         -- $home/.config/marksman/config.toml :
+  --         -- [core]
+  --         -- markdown.file_extensions = ["md", "markdown", "qmd"]
+  --         filetypes = { "markdown", "quarto" },
+  --         root_dir = require("lspconfig.util").root_pattern(".git", ".marksman.toml", "_quarto.yml"),
+  --       },
+  --     },
+  --   },
+  -- },
+  { "rafamadriz/friendly-snippets" },
 
+  -- Snippet Courtesy of @Zeioth,
+
+  {
+    "L3MON4D3/LuaSnip",
+    build = vim.fn.has("win32") ~= 0 and "make install_jsregexp" or nil,
+    dependencies = {
+      "rafamadriz/friendly-snippets",
+      "benfowler/telescope-luasnip.nvim",
+    },
+    config = function(_, opts)
+      if opts then
+        require("luasnip").config.setup(opts)
+      end
+      vim.tbl_map(function(type)
+        require("luasnip.loaders.from_" .. type).lazy_load()
+      end, { "vscode", "snipmate", "lua" })
+      -- friendly-snippets - enable standardized comments snippets
+      require("luasnip").filetype_extend("typescript", { "tsdoc" })
+      require("luasnip").filetype_extend("javascript", { "jsdoc" })
+      require("luasnip").filetype_extend("lua", { "luadoc" })
+      require("luasnip").filetype_extend("python", { "pydoc" })
+      -- require("luasnip").filetype_extend("rust", { "rustdoc" })
+      -- require("luasnip").filetype_extend("cs", { "csharpdoc" })
+      -- require("luasnip").filetype_extend("java", { "javadoc" })
+      -- require("luasnip").filetype_extend("c", { "cdoc" })
+      require("luasnip").filetype_extend("cpp", { "cppdoc" })
+      -- require("luasnip").filetype_extend("php", { "phpdoc" })
+      -- require("luasnip").filetype_extend("kotlin", { "kdoc" })
+      -- require("luasnip").filetype_extend("ruby", { "rdoc" })
+      require("luasnip").filetype_extend("sh", { "shelldoc" })
+    end,
+  },
   {
     "nvim-treesitter/nvim-treesitter",
     opts = {
