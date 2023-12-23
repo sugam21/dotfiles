30c819cd1b43191c21ef4b5926d79a2a05a8cdd3
sugam <sugams342@gmail.com>
2023-12-23 20:24:40 +0530

    Added nvim surround

--- nvim/.config/nvim/lua/plugins/ui.lua
+++ nvim/.config/nvim/lua/plugins/ui.lua
@@ -75,4 +75,14 @@ return {
     --    vim.cmd([[let g:mkdp_refresh_slow = 0]]),
     -- end
   },
+  {
+    "kylechui/nvim-surround",
+    version = "*", -- Use for stability; omit to use `main` branch for the latest features
+    event = "VeryLazy",
+    config = function()
+      require("nvim-surround").setup({
+        -- Configuration here, or leave empty to use defaults
+      })
+    end,
+  },
 }
